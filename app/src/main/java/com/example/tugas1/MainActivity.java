0a25e284ad4c395f0010dc0b453e0d10e9544f91
Nanda yudha pratama <nanda.yudha.ft18@mail.umy.ac.id>
2020-03-30 15:51:31 +0700

    activity 2

--- app/src/main/java/com/example/tugas1/MainActivity.java
+++ app/src/main/java/com/example/tugas1/MainActivity.java
@@ -1,6 +1,13 @@
 package com.example.tugas1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
 
 import android.os.Bundle;
 
@@ -10,5 +17,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is
+        present.
+                MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.optionmenu, menu);
+        return true;
+    }
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId()==R.id.inset){
+            startActivity(new Intent(this, InsertActivity.class));
+        } else if (item.getItemId() == R.id.edit) {
+            startActivity(new Intent(this, EditActivity.class));
+        }
+        return true;
     }
 }
